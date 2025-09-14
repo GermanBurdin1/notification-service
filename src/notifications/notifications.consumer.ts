63e97394e3d8926ba85c8daae71d25acc80056e1
German <125369843+GermanBurdin1@users.noreply.github.com>
2025-09-14 21:58:47 +0200

    Remove ESLint configuration file, add new ESLint config, introduce Docker Compose setup for notifications service and RabbitMQ, and implement CI/CD workflow for deployment.

--- src/notifications/notifications.consumer.ts
+++ src/notifications/notifications.consumer.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable} from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 
